d7d1d54b97108c772a4b9defdb687d9ff2e5c346
kosmetikJOT <joseolarte.kosmetikon@gmail.com>
2023-07-11 13:57:20 -0500

    Implementando barra de buscar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 //Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatInputModule
+    //Material
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSidenavModule
   ],
   exports:[
-    MatInputModule
+    //Material
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSidenavModule
   ]
 })
 export class SharedModule { }
